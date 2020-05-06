8b0ef23392b4d8eee286841c6f708b1b092edc0c
ddanijel <danijel.fon@gmail.com>
2020-05-06 16:48:36 +0200

    project init

---
+++ src/main/java/com/iotdatamp/entitymanager/MpEntityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.iotdatamp.entitymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpEntityManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpEntityManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iotdatamp/entitymanager/MpEntityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotdatamp.entitymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpEntityManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
